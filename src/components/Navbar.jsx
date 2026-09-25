6a071e35ef0a789225b518d97468c5f426ea8569
Wilson Muriithi <wilsonmuriithiwangu@gmail.com>
2026-09-25 08:27:49 -0700

    updated website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,99 +1,52 @@
 import { Link } from "react-router-dom";
-import { ShoppingBag, Menu, X } from "lucide-react";
-import { useState } from "react";
+import { ShoppingBag } from "lucide-react";
 import { useCart } from "../CartContext";
-import logo from "../assets/logo.jpg";
 
 function Navbar() {
-const [menuOpen, setMenuOpen] = useState(false);
-const { cartCount } = useCart();
+  const { cartCount } = useCart();
 
-const closeMenu = () => {
-setMenuOpen(false);
-};
+  return (
+    <header className="navbar">
+      <div className="navbar-inner">
 
-return (
-<header className="navbar">
-<div className="navbar-inner">
-<Link to="/" className="logo-link" onClick={closeMenu} >
-<img src={logo} alt="Thrift by Njeri" className="navbar-logo" />
-</Link>
+        {/* Logo - hidden on phones */}
+        <Link to="/" className="logo-link">
+          <img
+            src="/logo.jpg"
+            alt="Thrift by Njeri"
+            className="navbar-logo"
+          />
+        </Link>
 
-    <nav
-      className={
-        menuOpen
-          ? "nav-links active"
-          : "nav-links"
-      }
-    >
-      <Link to="/" onClick={closeMenu}>
-        Home
-      </Link>
+        {/* Navigation */}
+        <nav className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/shop">Shop</Link>
+          <Link to="/about">About</Link>
+          <Link to="/how-to-order">How to Order</Link>
+          <Link to="/admin">Admin</Link>
+        </nav>
 
-      <Link to="/shop" onClick={closeMenu}>
-        Shop
-      </Link>
+        {/* Shopping Bag */}
+        <div className="navbar-actions">
+          <Link
+            to="/cart"
+            className="bag-button"
+            aria-label={`Shopping cart with ${cartCount} items`}
+          >
+            <ShoppingBag size={20} />
 
-      <Link to="/about" onClick={closeMenu}>
-        About
-      </Link>
+            {cartCount > 0 && (
+              <span className="cart-count">
+                {cartCount}
+              </span>
+            )}
+          </Link>
+        </div>
 
-      <Link
-        to="/how-to-order"
-        onClick={closeMenu}
-      >
-        How to Order
-      </Link>
-
-      <Link to="/admin" onClick={closeMenu}>
-        Admin
-      </Link>
-    </nav>
-
-    <div className="navbar-actions">
-      <Link
-        to="/cart"
-        className="bag-button"
-        aria-label={
-          "Shopping cart with " +
-          cartCount +
-          " items"
-        }
-        onClick={closeMenu}
-      >
-        <ShoppingBag size={20} />
-
-        {cartCount > 0 && (
-          <span className="cart-count">
-            {cartCount}
-          </span>
-        )}
-      </Link>
-
-      <button
-        type="button"
-        className="menu-button"
-        onClick={() =>
-          setMenuOpen((current) => !current)
-        }
-        aria-label={
-          menuOpen
-            ? "Close navigation menu"
-            : "Open navigation menu"
-        }
-        aria-expanded={menuOpen}
-      >
-        {menuOpen ? (
-          <X size={24} />
-        ) : (
-          <Menu size={24} />
-        )}
-      </button>
-    </div>
-  </div>
-</header>
-
-);
+      </div>
+    </header>
+  );
 }
 
 export default Navbar;
